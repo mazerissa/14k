e0be23797ad160dedbc3178d7483b31fbfad20fc
mazerissa <youcefelghoul@yahoo.com>
2026-08-11 17:13:59 +0200

    importing files

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { router } from "./engine/router.js";
 import { generator } from "./engine/generator.js";
+import { renderer } from "./engine/renderer.js";
 
 const app = document.getElementById("app");
 
 const route = router();
 const page = generator(route);
 
-app.textContent = JSON.stringify(page, null, 2);
+renderer(page, app);
